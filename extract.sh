50cd065b0126c774ceab7296326fd9aee9ccd3bc
Tristan <kraig.tri125@gmail.com>
2017-02-04 20:23:44 -0500

    Fixup because 1.4 didn't have the module functionality.

--- extract.sh
+++ extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if test -r "$1" -a -f "$1"
 	then
-		cat "$1" | jq --slurp --compact-output '[.[] | {faction: .faction} ] | unique | .[]' > factions.jsonl
-		cat "$1" | jq --slurp --compact-output '[.[] | {type: .type} ] | unique | .[] | select(.type !=null)' > types.jsonl
-		cat "$1" | jq --slurp --compact-output '[.[] | {rarity: .variations[].rarity} ] | unique | .[]' > rarities.jsonl
-		cat "$1" | jq --slurp --compact-output '[.[] | .category] | flatten | unique | to_entries | .[] | select(.value !=null) | {category: .value}' > categories.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {faction: .faction} ] | unique | .[]' > factions.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {type: .type} ] | unique | .[] | select(.type !=null)' > types.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {rarity: .variations[].rarity} ] | unique | .[]' > rarities.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | .category] | flatten | unique | to_entries | .[] | select(.value !=null) | {category: .value}' > categories.jsonl
 else
 	echo "Error: File doesn't exists or can't read file."
 fi
