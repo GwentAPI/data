72b2142467dffca7b703192f05a5fc64312714b7
Tristan <kraig.tri125@gmail.com>
2017-02-04 20:51:43 -0500

    Correctly named fields and files for the generated jsonl.

--- extract.sh
+++ extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if test -r "$1" -a -f "$1"
 	then
-		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {faction: .faction} ] | unique | .[]' > factions.jsonl
-		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {type: .type} ] | unique | .[] | select(.type !=null)' > types.jsonl
-		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {rarity: .variations[].rarity} ] | unique | .[]' > rarities.jsonl
-		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | .category] | flatten | unique | to_entries | .[] | select(.value !=null) | {category: .value}' > categories.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {name: .faction} ] | unique | .[]' > factions.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {name: .type} ] | unique | .[] | select(.name !=null)' > 'groups.jsonl'
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {name: .variations[].rarity} ] | unique | .[]' > rarities.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | .category] | flatten | unique | to_entries | .[] | select(.value !=null) | {name: .value}' > categories.jsonl
 else
 	echo "Error: File doesn't exists or can't read file."
 fi
