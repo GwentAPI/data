23afeadc509dc3fbd22ffcec09a2d1f4e2bc5f6c
Tristan <kraig.tri125@gmail.com>
2017-02-05 22:46:31 -0500

    Fixup with the field rename that prevented jq from correctly creating categories.jsonl.

--- extract.sh
+++ extract.sh
@@ -4,7 +4,7 @@ if test -r "$1" -a -f "$1"
 		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {name: .faction} ] | unique | .[]' > factions.jsonl
 		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {name: .type} ] | unique | .[] | select(.name !=null)' > 'groups.jsonl'
 		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | {name: .variations[].rarity} ] | unique | .[]' > rarities.jsonl
-		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | .category] | flatten | unique | to_entries | .[] | select(.value !=null) | {name: .value}' > categories.jsonl
+		cat "$1" | jq --slurp --compact-output 'def flatten: reduce .[] as $i ([]; if $i | type == "array" then . + ($i | flatten) else . + [$i] end); [.[] | .categories] | flatten | unique | to_entries | .[] | select(.value !=null) | {name: .value}' > categories.jsonl
 else
 	echo "Error: File doesn't exists or can't read file."
 fi
